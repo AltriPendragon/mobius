37c76f57a23e50fe6c2755ae34941f9a1e8dbb0f
ricefan <altri@foxmail.com>
2024-03-24 13:27:49 +0800

    init

---
+++ build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.4"
+	id("io.spring.dependency-management") version "1.1.4"
+	id("org.graalvm.buildtools.native") version "0.10.1"
+}
+
+group = "com.altri"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://maven.aliyun.com/nexus/content/groups/public/") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+pluginManagement {
+	repositories {
+		maven { url = uri("https://maven.aliyun.com/nexus/content/groups/public/") }
+		gradlePluginPortal()
+	}
+}
+rootProject.name = "mobius"
